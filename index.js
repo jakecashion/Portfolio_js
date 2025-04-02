3466a364e2547f3c909b354c005961d9bb5e03c3
jakecashion <jakecashion98@gmail.com>
2025-04-01 20:49:48 -0400

    html, css, and java
    Updated html, css, and java (text animation). Linked boxicons, and fonts.

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-texts", {
+    strings: ["Frontend developer", "CS Student", "Open Source Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+})
